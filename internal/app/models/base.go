1c24d6d5de5688aa1bb1d49f3e595138d0b03d03
zhouyuan63 <zhouyuan11@jd.com>
2023-02-01 18:11:33 +0800

    up

--- internal/app/models/base.go
+++ internal/app/models/base.go
@@ -4,9 +4,10 @@ import (
 	"time"
 )
 
+// gorm doc: https://gorm.io/
 type BaseModel struct {
-	Id           int       `gorm:"primary_key" json:"id"`
-	CreatedDate  time.Time `gorm:"type:timestamp not null;default:current_timestamp" json:"created_date"`
-	ModifiedDate time.Time `gorm:"type:timestamp on update current_timestamp;default:current_timestamp;" json:"modified_date"`
+	Id           int       `gorm:"primary_key;comment:'id'" json:"id"`
+	CreatedDate  time.Time `gorm:"comment:'创建日期';type:timestamp not null;default:current_timestamp" json:"created_date"`
+	ModifiedDate time.Time `gorm:"comment:'修改日期';type:timestamp on update current_timestamp;" json:"modified_date"`
 	Yn           int8      `gorm:"default:1;comment:'是否删除: 1存在 2删除'" json:"yn"`
 }
--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -2,11 +2,15 @@ package models
 
 type User struct {
 	BaseModel
-	NickName string `json:"nick_name"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	NickName string `json:"nick_name" gorm:"type:varchar(50);comment:'昵称'"`
+	Name     string `json:"name" gorm:"type:varchar(50);comment:'姓名'"`
+	Password string `json:"password" gorm:"type:varchar(50);comment:'密码'"`
 	Role     int    `gorm:"size:255;default:1" json:"role"`
-	Email    string `json:"email"`
+	Email    string `json:"email" gorm:"type:varchar(250);comment:'邮箱'"`
+}
+
+func (u User) Comment() string {
+	return "用户表"
 }
 
 func ChenkToken(name, token string) (*User, error) {
