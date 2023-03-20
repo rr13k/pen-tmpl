3a0bd1fec73c64d4de405303c675ce6c19faa56f
zhouyuan63 <zhouyuan11@jd.com>
2023-03-20 11:18:07 +0800

    up

--- internal/app/models/base.go
+++ internal/app/models/base.go
@@ -8,6 +8,6 @@ import (
 type BaseModel struct {
 	Id           int       `gorm:"primary_key;comment:'id'" json:"id"`
 	CreatedDate  time.Time `gorm:"comment:'创建日期';type:timestamp not null;default:current_timestamp" json:"created_date"`
-	ModifiedDate time.Time `gorm:"comment:'修改日期';type:timestamp on update current_timestamp;" json:"modified_date"`
+	ModifiedDate time.Time `gorm:"comment:'修改日期'; autoUpdateTime" json:"modified_date"`
 	Yn           int8      `gorm:"default:1;comment:'是否删除: 1存在 2删除'" json:"yn"`
 }
