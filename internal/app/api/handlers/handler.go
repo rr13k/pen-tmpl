13bcd3092c0122e69251619e6773020b543d41c9
zhouyuan63 <zhouyuan11@jd.com>
2023-05-12 20:55:31 +0800

    up

--- internal/app/api/handlers/handler.go
+++ internal/app/api/handlers/handler.go
@@ -2,14 +2,6 @@ package handlers
 
 import "net/http"
 
-type BaseResponse struct {
-	Success bool
-	Code    int
-	Message string
-	Data    interface{}
-	Total   int `json:"total"`
-}
-
 func Ping(w http.ResponseWriter, r *http.Request) {
 	data := []byte("pong")
 	w.Write(data)
