8d0255903154d888780db7839e8e493609d83bb6
zhouyuan63 <zhouyuan11@jd.com>
2023-05-17 11:22:04 +0800

    up

--- internal/app/api/handlers/user_test.go
+++
@@ -1,5 +0,0 @@
-package handlers
-
-func Test_nihao() {
-
-}
--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -8,33 +8,3 @@ type User struct {
 	Role     int    `gorm:"size:255;default:1" json:"role"`
 	Email    string `json:"email" gorm:"type:varchar(250);comment:'邮箱'"`
 }
-
-func (u User) Comment() string {
-	return "用户表"
-}
-
-func ChenkToken(name, token string) (*User, error) {
-	var u User
-	err := DB.Model(&User{}).Where("yn", 1).Where("name", name).Where("password", token).Find(&u).Error
-	return &u, err
-}
-
-func GetUser(maps interface{}) (*User, error) {
-	var _user *User
-	err := DB.Model(&User{}).Where(maps).Find(&_user).Error
-	return _user, err
-}
-
-func GetAllUser(pageNum int, pageSize int, maps map[string]interface{}) ([]*User, error) {
-	var _users []*User
-	err := WhereMap(DB, maps).Model(&User{}).Offset(pageNum).Limit(pageSize).Find(&_users).Error
-	return _users, err
-}
-
-func GetUserTotal(maps map[string]interface{}) (int, error) {
-	var count int64
-	if err := WhereMap(DB, maps).Model(&User{}).Count(&count).Error; err != nil {
-		return 0, err
-	}
-	return int(count), nil
-}
